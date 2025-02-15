04f2c351f9c87af8492c66b7475eb8b9f9350308
Ashwin <ashwinb1234@gmail.com>
2025-02-15 14:18:10 -0500

    Update visualizer.py

--- assignment2/visualizer.py
+++ assignment2/visualizer.py
@@ -22,6 +22,7 @@ def mesh_visualizer(mesh, output_path = 'mesh_visualize.gif', textures= None,
                     number_views= 20, image_size=256, distance= 3, fov =60, 
                     fps=12, elev=1):
     device = get_device()
+    print(device)
     vertices = mesh.verts_list()[0]
     faces = mesh.faces_list()[0]
     vertices = vertices.unsqueeze(0)  # (N_v, 3) -> (1, N_v, 3)
@@ -45,6 +46,31 @@ def mesh_visualizer(mesh, output_path = 'mesh_visualize.gif', textures= None,
     lights = pytorch3d.renderer.PointLights(location=[[0, 0, 3]], device=device)
     
     images = renderer(render_mesh.extend(number_views), cameras= cameras, lights= lights)
+    # images = images.cpu().numpy()[..., :3]
+    # images = (images * 255).clip(0, 255).astype(np.uint8)
+    my_images = my_images.cpu().detach().numpy()
+    imageio.mimsave(output_path, images, fps=fps, format='gif', loop=0)
+    
+    return
+
+def pointcloud_visualizer(pcd, output_path = 'pointcloud_visualize.gif', textures= None,
+                    number_views= 20, image_size=256, distance= 3, fov =60, 
+                    fps=12, elev=1):
+    device = get_device()
+    points = pcd
+    if rgb==None:
+      rgb = (points - points.min()) / (points.max() - points.min())
+
+    point_cloud = pytorch3d.structures.Pointclouds(points=[points], 
+                                                   features=[rgb],).to(device)
+    azimuth = np.linspace(-180, 180, num=number_views)
+    R, T = pytorch3d.renderer.look_at_view_transform(dist = distance, elev = elev, 
+                                                     azim =azimuth)
+    cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, fov= fov, device=device)
+    renderer = get_points_renderer(image_size=image_size, device=device)
+    lights = pytorch3d.renderer.PointLights(location=[[0, 0, 3]], device=device)
+    
+    images = renderer(point_cloud.extend(number_views), cameras= cameras, lights= lights)
     # images = images.cpu().numpy()[..., :3]
     # images = (images * 255).clip(0, 255).astype(np.uint8)
     my_images = my_images.cpu().detach().numpy()
