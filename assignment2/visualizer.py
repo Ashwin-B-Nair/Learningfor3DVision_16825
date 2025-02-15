651d7cbdaa407cc7ba33755c59ba3c7e1ca8544e
Ashwin <ashwinb1234@gmail.com>
2025-02-15 14:38:24 -0500

    Update visualizer.py

--- assignment2/visualizer.py
+++ assignment2/visualizer.py
@@ -48,7 +48,7 @@ def mesh_visualizer(mesh, output_path = 'mesh_visualize.gif', textures= None,
     images = renderer(render_mesh.extend(number_views), cameras= cameras, lights= lights)
     # images = images.cpu().numpy()[..., :3]
     # images = (images * 255).clip(0, 255).astype(np.uint8)
-    my_images = my_images.cpu().detach().numpy()
+    images = images.cpu().detach().numpy()
     imageio.mimsave(output_path, images, fps=fps, format='gif', loop=0)
     
     return
