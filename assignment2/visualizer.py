8c560381bdd06bbe0b7c575f904b4e846f3a0b1f
Ashwin <ashwinb1234@gmail.com>
2025-02-19 11:42:58 -0500

    Update visualizer.py

--- assignment2/visualizer.py
+++ assignment2/visualizer.py
@@ -37,11 +37,18 @@ def mesh_visualizer(mesh, output_path = 'mesh_visualize.gif', textures= None,
     if textures is None:
         # textures = torch.ones_like(vertices)  # (1, N_v, 3)
         # textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
+        
+        # if vertices.numel() > 0:
+        #     textures = torch.ones_like(vertices)
+        #     textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
+        # else:
+        #     textures = torch.ones_like(vertices)  # Default to zero textures for empty tensors
+
         if vertices.numel() > 0:
             textures = torch.ones_like(vertices)
-            textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
         else:
-            textures = torch.ones_like(vertices)  # Default to zero textures for empty tensors
+            print("Vertices are empty; using default zero textures.")
+            textures = torch.zeros(1, 1, 3)
     
     render_mesh = pytorch3d.structures.Meshes(
             verts=vertices,
