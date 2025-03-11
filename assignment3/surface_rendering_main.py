c9d0ad22d36e2a0c52fee60d659127bf437e1f99
Ashwin <ashwinb1234@gmail.com>
2025-03-11 13:16:32 -0400

    Update surface_rendering_main.py

--- assignment3/surface_rendering_main.py
+++ assignment3/surface_rendering_main.py
@@ -233,7 +233,7 @@ def train_points(
 
             # Get distances and enforce point cloud loss
             distances, gradients = model.implicit_fn.get_distance_and_gradient(points)
-            loss = None # TODO (Q6): Point cloud SDF loss on distances
+            loss = torch.mean(torch.linalg.norm(distances, dim=-1)) # TODO (Q6): Point cloud SDF loss on distances
             point_loss = loss
 
             # Sample random points in bounding box
