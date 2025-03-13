3f04ffd76690c82a70a6e832fdcb4c939601f746
Ashwin <ashwinb1234@gmail.com>
2025-03-12 21:17:25 -0400

    update

--- assignment3/volume_rendering_main.py
+++ assignment3/volume_rendering_main.py
@@ -367,7 +367,7 @@ def train_nerf(
                     model, create_surround_cameras(4.0, n_poses=20, up=(0.0, 0.0, 1.0), focal_length=2.0),
                     cfg.data.image_size, file_prefix='nerf'
                 )
-                imageio.mimsave(f'images/part_8_NeRF_50view.gif', [np.uint8(im * 255) for im in test_images], loop=0)
+                imageio.mimsave(f'images/part_8_NeRF_10view.gif', [np.uint8(im * 255) for im in test_images], loop=0)
 
 
 @hydra.main(config_path='./configs', config_name='sphere')
